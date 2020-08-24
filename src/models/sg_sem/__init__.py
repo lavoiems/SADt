c7800ddef5597c24d574c63e1db93dd07627f41c
Sam <samuel.lavoie.m@gmail.com>
2020-08-24 10:11:31 -0400

    load last model cluster

--- src/models/sg_sem/__init__.py
+++ src/models/sg_sem/__init__.py
@@ -19,7 +19,7 @@ def execute(args):
     print(args)
 
     solver = Solver(args)
-    semantics = model.semantics(args.ss_path, args.cluster_path, args.cluster_type)
+    semantics = model.semantics(args.ss_path, args.cluster_type, args.cluster_path)
     semantics = semantics.to(args.device)
     semantics.eval()
 
