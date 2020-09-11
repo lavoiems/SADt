de389abe3387cb3107c3fbf4d932e97f88595c65
Sam <samuel.lavoie.m@gmail.com>
2020-09-11 11:11:10 -0400

    sg_sem_cont

--- src/models/imsat/train.py
+++ src/models/imsat/train.py
@@ -128,9 +128,7 @@ def train(args):
 
             optim_contrastive.zero_grad()
             ploss, nloss, gp = contrastive_loss(datax, args.n_classes, encoder, contrastive, args.device)
-            ploss.backward()
-            nloss.backward(mone)
-            (1 * gp).backward()
+            (ploss - nloss + gp).backward()
             optim_contrastive.step()
 
         optim_encoder.zero_grad()
